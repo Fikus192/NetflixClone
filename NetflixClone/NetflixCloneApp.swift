4623b1d0c28d3c0c16bc6a4a5aa7fcb757cc8523
Mateusz Ratajczak <sanmateusz@gmail.com>
2024-07-25 17:09:47 +0200

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Kasa 3 - Tychy on 25/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
