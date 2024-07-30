ee000b2c67320a6ac1d32150c6304ce5783c77e4
Mateusz Ratajczak <sanmateusz@gmail.com>
2024-07-30 19:13:48 +0200

    [Feature] Add Colors and Color extensions

---
+++ NetflixClone/Utilities/Color+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Color+Extensions.swift
+//  NetflixClone
+//
+//  Created by Mateusz Ratajczak on 30/07/2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    let customBlack = Color("CustomBlack")
+    let customDarkGray = Color("CustomDarkGray")
+    let customLightGray = Color("CustomLightGray")
+    let customDarkRed = Color("CustomDarkRed")
+    let customRed = Color("CustomRed")
+    let customWhite = Color("CustomWhite")
+}
