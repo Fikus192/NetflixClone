45c6b5acb55b10e2b7a5f6d7b5d08d907f830568
Mateusz Ratajczak <sanmateusz@gmail.com>
2024-08-02 19:25:47 +0200

    [Feature] Add FrameReader component

---
+++ NetflixClone/Components/FrameReader.swift
@@ -0,0 +1,38 @@
+//
+//  FrameReader.swift
+//  NetflixClone
+//
+//  Created by Mateusz Ratajczak on 02/08/2024.
+//
+
+import SwiftUI
+
+public struct FrameReader: View {
+    
+    let coordinateSpace: CoordinateSpace
+    let onChange: (_ frame: CGRect) -> Void
+    
+    public init(coordinateSpace: CoordinateSpace, onChange: @escaping (_ frame: CGRect) -> Void) {
+        self.coordinateSpace = coordinateSpace
+        self.onChange = onChange
+    }
+    
+    public var body: some View {
+        GeometryReader { geo in
+            Text("")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .onAppear(perform: {
+                    onChange(geo.frame(in: coordinateSpace))
+                })
+                .onChange(of: geo.frame(in: coordinateSpace), perform: onChange)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+public extension View {
+    
+    func readingFrame(coordinateSpace: CoordinateSpace = .global, onChange: @escaping (_ frame: CGRect) -> ()) -> some View {
+        background(FrameReader(coordinateSpace: coordinateSpace, onChange: onChange))
+    }
+}
