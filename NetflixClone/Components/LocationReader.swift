6cbd5f82e299509da3b4bea11c1a3aef5a74776e
Mateusz Ratajczak <sanmateusz@gmail.com>
2024-08-07 17:57:51 +0200

    [Feature] Implement LocationReader by SwiftfulThinking

---
+++ NetflixClone/Components/LocationReader.swift
@@ -0,0 +1,26 @@
+//
+//  LocationReader.swift
+//  NetflixClone
+//
+//  Created by Mateusz Ratajczak on 07/08/2024.
+//
+
+import SwiftUI
+
+public struct LocationReader: View {
+    
+    let coordinateSpace: CoordinateSpace
+    let onChange: (_ location: CGPoint) -> Void
+
+    public init(coordinateSpace: CoordinateSpace, onChange: @escaping (_ location: CGPoint) -> Void) {
+        self.coordinateSpace = coordinateSpace
+        self.onChange = onChange
+    }
+    
+    public var body: some View {
+        FrameReader(coordinateSpace: coordinateSpace) { frame in
+            onChange(CGPoint(x: frame.midX, y: frame.midY))
+        }
+        .frame(width: 0, height: 0, alignment: .center)
+    }
+}
