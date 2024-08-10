d6679585d76548f1be0604c55ac8c4cadcf56cce
Mateusz Ratajczak <sanmateusz@gmail.com>
2024-08-10 16:13:51 +0200

    [Feature] Add DetailsHeaderView()

--- NetflixClone/Components/DetailsHeaderView.swift
+++ NetflixClone/Components/DetailsHeaderView.swift
@@ -15,8 +15,51 @@ struct DetailsHeaderView: View {
     var onXMarkPressed: (() -> Void)?
 
     var body: some View {
-        ZStack {
+        ZStack(alignment: .bottom) {
             ImageLoaderView(urlString: imageName)
+            
+            CustomProgressBar(
+                selection: progress,
+                range: 0...1,
+                backgroundColor: Color.theme.customLightGray,
+                foregroundColor: Color.theme.customRed,
+                cornerRadius: 2,
+                height: 4
+            )
+            .padding(.bottom, 4)
+            .animation(.linear, value: progress)
+            
+            HStack(spacing: 8) {
+                Button {
+                    onAirplayPressed?()
+                } label: {
+                    Circle()
+                        .fill(Color.theme.customDarkGray)
+                        .overlay(
+                            Image(systemName: "tv.badge.wifi")
+                                .offset(y: 1)
+                        )
+                        .frame(width: 36, height: 36)
+                }
+                
+                Button {
+                    onXMarkPressed?()
+                } label: {
+                    Circle()
+                        .fill(Color.theme.customDarkGray)
+                        .overlay(
+                            Image(systemName: "xmark")
+                                .offset(y: 1)
+                        )
+                        .frame(width: 36, height: 36)
+                }
+
+            }
+            .foregroundStyle(Color.theme.customWhite)
+            .font(.subheadline)
+            .fontWeight(.bold)
+            .padding(8)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
         }
         .aspectRatio(2, contentMode: .fit)
     }
