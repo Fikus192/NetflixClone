e11509569945312d9be5d8c9af0c16104ad9903a
Mateusz Ratajczak <sanmateusz@gmail.com>
2024-08-16 14:31:30 +0200

    [Feature] Implement Buttons and MovieRowView into MovieDetailsView()

--- NetflixClone/Views/MovieDetailsView.swift
+++ NetflixClone/Views/MovieDetailsView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct MovieDetailsView: View {
     
+    @StateObject private var vm = HomeViewModel()
+    
     var product: Product = .mock
+    
     @State private var progress: Double = 0.2
+    @State private var isMyList: Bool = false
+    @State private var products: [Product] = []
     
     var body: some View {
         ZStack {
@@ -33,7 +38,7 @@ struct MovieDetailsView: View {
                 )
                 
                 ScrollView(.vertical) {
-                    VStack(spacing: 16) {
+                    VStack(alignment: .leading, spacing: 16) {
                         DetailsProductView(
                             title: product.title,
                             isNew: true,
@@ -50,11 +55,59 @@ struct MovieDetailsView: View {
                                 
                             }
                         )
+                        
+                        HStack(spacing: 32) {
+                            MyListButton(isMyList: isMyList) {
+                                isMyList.toggle()
+                            }
+                            
+                            RateButton { selection in
+                                // do something with selection
+                            }
+                            
+                            ShareButton()
+                        }
+                        .padding(.leading, 32)
+                        
+                        VStack(alignment: .leading) {
+                            Text("More Like This")
+                                .font(.headline)
+                            
+                            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 8), count: 3),
+                                      alignment: .center,
+                                      spacing: 8,
+                                      pinnedViews: [],
+                                      content: {
+                                ForEach(products) { product in
+                                    MovieRowView(
+                                        imageName: product.firstImage,
+                                        title: product.title,
+                                        isRecentlyAdded: product.recentlyAdded,
+                                        topTenRanking: nil
+                                    )
+                                }
+                            })
+                        }
+                        .foregroundStyle(Color.theme.customWhite)
                     }
                     .padding(8)
                 }
                 .scrollIndicators(.hidden)
             }
+        }
+        .task {
+            await getData()
+        }
+        .toolbar(.hidden, for: .navigationBar)
+    }
+    
+    private func getData() async {
+        guard products.isEmpty else { return }
+        
+        do {
+            products = try await DatabaseHelper().getProducts()
+        } catch {
+            
         }
     }
 }
